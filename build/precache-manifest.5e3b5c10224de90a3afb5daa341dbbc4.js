6cbc9e1f56c2951e140825b002e0108d5ed141de
Kaveh <kaveh.ka@gmail.com>
2019-01-17 15:35:25 -0800

    Init repo

---
+++ build/precache-manifest.5e3b5c10224de90a3afb5daa341dbbc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d9ce7cca86e31313e32",
+    "url": "/static/js/main.3d9ce7cc.chunk.js"
+  },
+  {
+    "revision": "7f68df6f4d2b157d1ba5",
+    "url": "/static/js/1.7f68df6f.chunk.js"
+  },
+  {
+    "revision": "7f68df6f4d2b157d1ba5",
+    "url": "/static/css/1.9b73767f.chunk.css"
+  },
+  {
+    "revision": "577404eb3abb8e05062ca01dad73c670",
+    "url": "/index.html"
+  }
+];
